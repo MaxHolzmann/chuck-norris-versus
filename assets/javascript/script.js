28a3d65c164d74893caf1bf1521c77a3c74ece8a
Maximilian Holzmann <max@holzmann.io>
2022-12-01 20:36:52 -0600

    display joke function and script link in index

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -17,3 +17,15 @@ getChuckNorris
 displayJoke()
 
 */
+
+
+
+const displayJoke = (chuck, dad) => {
+    let chuckJoke = document.getElementById('chuck-joke');
+    let dadJoke = document.getElementById('dad-joke');
+    chuck = "test";
+    dad = "test2"
+
+    chuckJoke.textContent = chuck;
+    dadJoke.textContent = dad;
+}
